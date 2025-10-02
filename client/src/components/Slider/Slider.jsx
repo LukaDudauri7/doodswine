7055b030783198914323d44d9d94d9358ed5a888
LukaDudauri7 <lukadudauri01@gmail.com>
2025-10-02 12:13:22 +0400

    .

--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -7,7 +7,7 @@ import 'swiper/css/pagination';
 import './Slider.css';
 
 const Slider = () => {
-  const imageCount = 6;
+  const imageCount = 7;
   const images = Array.from({ length: imageCount }, (_, i) => `/images/wine${i + 1}.jpg`);
   return (
     <div className="slider-container">
