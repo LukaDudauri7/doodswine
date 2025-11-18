b593045e563a3a11ce90faacb63a573f41f0008f
LukaDudauri7 <lukadudauri01@gmail.com>
2025-11-18 17:51:02 +0400

    new APP URL

--- client/src/components/AuthModal/AuthModal.jsx
+++ client/src/components/AuthModal/AuthModal.jsx
@@ -7,7 +7,8 @@ function AuthModal({ type, onClose, setUser }) {
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
-    const API = process.env.REACT_APP_API_URL || "http://localhost:5000";
+    //const API = process.env.REACT_APP_API_URL || "http://localhost:5000";
+    const API = process.env.REACT_APP_API_URL || "https://doodswine.onrender.com";
   
   const handleSubmit = async (e) => {
     e.preventDefault();
