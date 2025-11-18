31ad338b81b4d3a8def909b4ffc92340138f4843
LukaDudauri7 <lukadudauri01@gmail.com>
2025-11-18 18:03:00 +0400

    add default port

--- backend/server.js
+++ backend/server.js
@@ -23,5 +23,5 @@ app.use('/api/label', require('./routes/label'));
 app.use("/uploads", express.static("uploads"));
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
