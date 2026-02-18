49711bc9c6ca565696ecf863bf27933ef1322490
Luka <lukadudauri01@gmail.com>
2026-02-18 22:59:28 +0400

    remove btn

--- client/src/components/BottleDesign/BottleDesign.jsx
+++ client/src/components/BottleDesign/BottleDesign.jsx
@@ -164,7 +164,7 @@ const BottleDesign = ({ user, openModal }) => {
 
               {labelImageBase64 && (
                 <button className="remove-image-btn" onClick={removeImage}>
-                  {content.removeImage}
+                  ✕ {content.removeImage}
                 </button>
               )}
 
