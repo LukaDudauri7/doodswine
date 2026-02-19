7db943dd3690f25c28ff221873abae727909f975
Luka <lukadudauri01@gmail.com>
2026-02-19 12:27:51 +0400

    dvh

--- client/src/components/WineCustomizer/WineCustomizer.jsx
+++ client/src/components/WineCustomizer/WineCustomizer.jsx
@@ -133,6 +133,17 @@ function WineBottle({
     }
   }, []);
 
+  useEffect(() => {
+    const setVH = () => {
+      document.documentElement.style.setProperty("--vh", `${window.innerHeight * 0.01}px`);
+  };
+
+  setVH();
+    window.addEventListener("resize", setVH);
+
+    return () => window.removeEventListener("resize", setVH);
+  }, []);
+
   /* ---------- DRAW LABEL ---------- */
 
   useEffect(() => {
